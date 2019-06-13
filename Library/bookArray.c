1f4e753c14113945cab1cd49783fe9512f007996
Guilhem CESAR <guilhem.cesar2@gmail.com>
2019-06-13 18:13:00 +0200

    fichier a bobol avec ligne doublée

--- Library/bookArray.c
+++ Library/bookArray.c
@@ -1,38 +1,145 @@
+//#define DyCzvGuO1CTg1CkpWRX0 IBookArray
+#define DyCzvGuO1CTg1CkpWRX0 IBookArray
+//#define uz91GaKHEGW8mLbZ9YK BookArray
+#define uz91GaKHEGW8mLbZ9YK BookArray
+//#define oUVhQ {
+#define oUVhQ {
+//#define HDKSP }
+#define HDKSP }
+//#define bUn7lgtWyIXF9K6JK09O bookaInsertAt
+#define bUn7lgtWyIXF9K6JK09O bookaInsertAt
+//#define pyWRUyKz void
+#define pyWRUyKz void
+//#define meWLrjd0nr if
+#define meWLrjd0nr if
+//#define pFaZ1NS2 int
+#define pFaZ1NS2 int
+//#define CBoAUqi0k7 bUn7lgtWyIXF9K6JK09O
+#define CBoAUqi0k7 bUn7lgtWyIXF9K6JK09O
+//#define SwtUV ;
+#define SwtUV ;
+//#define KSr4FIYcOI3YF4URXnzy NULL
+#define KSr4FIYcOI3YF4URXnzy NULL
+//#define kIuIgv2Ec0s8UnWCyauq  (
+#define kIuIgv2Ec0s8UnWCyauq  (
+//#define QgUwn9NAEHtLOya11dgx else
+#define QgUwn9NAEHtLOya11dgx else
+//#define Xk2US4zxPty1O8QDh *
+#define Xk2US4zxPty1O8QDh *
+//#define sWHYfMcmyBNcWNK pFaZ1NS2
+#define sWHYfMcmyBNcWNK pFaZ1NS2
+//#define sWHYfMcmyBNcWNK pFaZ1NS2
+#define PWL Xk2US4zxPty1O8QDh
+//#define PWL Xk2US4zxPty1O8QDh
+#define PWL Xk2US4zxPty1O8QDh
+//#define zz0 0
+#define zz0 0
+//#define pxexKwpiTjCuwzQM SwtUV HDKSP
+#define pxexKwpiTjCuwzQM SwtUV HDKSP
+//#define Klm9i2uEL7 return
+#define Klm9i2uEL7 return
+//#define GAeiCZduVBOeLgAvhRvg  )
+#define GAeiCZduVBOeLgAvhRvg  )
+//#define GXOERJfGJkjrQih pFaZ1NS2
+#define GXOERJfGJkjrQih pFaZ1NS2
+//#define W54itc2aQA meWLrjd0nr
+#define W54itc2aQA meWLrjd0nr
+//#define maTppu8CEeuizUPyLj9q =
+#define maTppu8CEeuizUPyLj9q =
+//#define rIcykUXL14JgWinydNhx maTppu8CEeuizUPyLj9q
+#define rIcykUXL14JgWinydNhx maTppu8CEeuizUPyLj9q
+//#define vryaIGqo7kdGDkRL7VZO struct
+#define vryaIGqo7kdGDkRL7VZO struct
+//#define JHrXMJSgdCMA4OjS6sT IBook
+#define JHrXMJSgdCMA4OjS6sT IBook
+//#define IOiDvOT2CQn GAeiCZduVBOeLgAvhRvg
+#define IOiDvOT2CQn GAeiCZduVBOeLgAvhRvg
+//#define ZiSMtT9hTVn83Awcj3G sizeof
+#define ZiSMtT9hTVn83Awcj3G sizeof
+//#define VSjteJVLmYrsEbdnwoTV QgUwn9NAEHtLOya11dgx
+#define VSjteJVLmYrsEbdnwoTV QgUwn9NAEHtLOya11dgx
+//#define r5UiRD67pBW2eNKY while
+#define r5UiRD67pBW2eNKY while
+//#define wFwBvQhzCHMkirj7 r5UiRD67pBW2eNKY
+#define wFwBvQhzCHMkirj7 r5UiRD67pBW2eNKY
+//#define Klm9i2EL7 Klm9i2uEL7
+#define Klm9i2EL7 Klm9i2uEL7
+//#define RrQlSvRxJluemLp bookaIndexOf
+#define RrQlSvRxJluemLp bookaIndexOf
+//#define qAYX4nd8Rb DyCzvGuO1CTg1CkpWRX0
+#define DyCzvGuO1CTg1CkpWRX0 DyCzvGuO1CTg1CkpWRX0
+//#define MLldsJqmq62yDYpKuP9X rIcykUXL14JgWinydNhx
+#define MLldsJqmq62yDYpKuP9X rIcykUXL14JgWinydNhx
+//#define SDBsAVbrbdlOACs8FB9 bookaRemoveAt
+#define SDBsAVbrbdlOACs8FB9 bookaRemoveAt
+//#define gY8GGIzIQZF45mZ61QfJ SwtUV
+#define gY8GGIzIQZF45mZ61QfJ SwtUV
+//#define Klm96i2EL7 Klm9i2uEL7
+#define Klm96i2EL7 Klm9i2uEL7
+//#define WQqLD3YU9E SDBsAVbrbdlOACs8FB9
+#define WQqLD3YU9E SDBsAVbrbdlOACs8FB9
+//#define Tc10Y bookaNew
+#define Tc10Y bookaNew
+//#define n3tI5vcrIPUuCKlUvu7i maTppu8CEeuizUPyLj9q
+#define n3tI5vcrIPUuCKlUvu7i maTppu8CEeuizUPyLj9q
+//#define hEiM8zGRYe Klm9i2uEL7
+#define hEiM8zGRYe Klm9i2uEL7
+//#define D8zMP bookaSize
+#define D8zMP bookaSize
+//#define uVO1Qalnv5iBgS7nlmK9 gY8GGIzIQZF45mZ61QfJ
+#define uVO1Qalnv5iBgS7nlmK9 gY8GGIzIQZF45mZ61QfJ
+//#define AILyGSaIaFLWs4kSC1p1 DyCzvGuO1CTg1CkpWRX0
+#define AILyGSaIaFLWs4kSC1p1 DyCzvGuO1CTg1CkpWRX0
+
 #include <assert.h>
 #include "bookArray.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
-IBookArray bookaNew() {
-	return NULL;
-}
+// bon courage. :)
 
-void bookaDelete(IBookArray this) {
-}
+vryaIGqo7kdGDkRL7VZO uz91GaKHEGW8mLbZ9YK oUVhQ JHrXMJSgdCMA4OjS6sT _0xb34519[0b00001010] gY8GGIzIQZF45mZ61QfJ vryaIGqo7kdGDkRL7VZO uz91GaKHEGW8mLbZ9YK Xk2US4zxPty1O8QDh aGuhyoaIeawjCrqEnIqj SwtUV  HDKSP gY8GGIzIQZF45mZ61QfJ
 
-void bookaAppend(IBookArray this, IBook book) {
-}
+DyCzvGuO1CTg1CkpWRX0 Tc10Y kIuIgv2Ec0s8UnWCyauq  GAeiCZduVBOeLgAvhRvg  oUVhQ
+    AILyGSaIaFLWs4kSC1p1 PJU3A5SyFt7FRR3JoRRA=calloc kIuIgv2Ec0s8UnWCyauq 1, ZiSMtT9hTVn83Awcj3G kIuIgv2Ec0s8UnWCyauq vryaIGqo7kdGDkRL7VZO uz91GaKHEGW8mLbZ9YK GAeiCZduVBOeLgAvhRvg  GAeiCZduVBOeLgAvhRvg  SwtUV
+    kIuIgv2Ec0s8UnWCyauq PWL PJU3A5SyFt7FRR3JoRRA GAeiCZduVBOeLgAvhRvg.aGuhyoaIeawjCrqEnIqj rIcykUXL14JgWinydNhx KSr4FIYcOI3YF4URXnzy SwtUV
+    Klm9i2EL7 PJU3A5SyFt7FRR3JoRRA gY8GGIzIQZF45mZ61QfJ  HDKSP
 
-IBook bookaGet(IBookArray this, int i) {
-	return NULL;
-}
+pyWRUyKz bookaDelete kIuIgv2Ec0s8UnWCyauq DyCzvGuO1CTg1CkpWRX0 vFrHnDaB8j9riC3ZrGDA GAeiCZduVBOeLgAvhRvg  oUVhQ
+    free kIuIgv2Ec0s8UnWCyauq vFrHnDaB8j9riC3ZrGDA GAeiCZduVBOeLgAvhRvg  pxexKwpiTjCuwzQM
 
-int bookaIndexOf(IBookArray this, IBook book) {
-	return 0;
-}
-
-void bookaInsertAt(IBookArray this, int i, IBook book) {
-}
-
-void bookaRemoveAt(IBookArray this, int i) {
-}
-
-void bookaRemoveLast(IBookArray this) {
-}
-
-void bookaSet(IBookArray this, int i, IBook book) {
-}
-
-int bookaSize(IBookArray this) {
-	return 0;
-}
+pyWRUyKz bookaAppend kIuIgv2Ec0s8UnWCyauq AILyGSaIaFLWs4kSC1p1 OWoBWpiptHwNh491gY38, JHrXMJSgdCMA4OjS6sT fJgdXHalMfeAVFwePOoI GAeiCZduVBOeLgAvhRvg  oUVhQ
+    sWHYfMcmyBNcWNK whiQ5 = 0 SwtUV
+    wFwBvQhzCHMkirj7  kIuIgv2Ec0s8UnWCyauq OWoBWpiptHwNh491gY38->_0xb34519[whiQ5] != NULL GAeiCZduVBOeLgAvhRvg  oUVhQ whiQ5++ gY8GGIzIQZF45mZ61QfJ HDKSP
+    OWoBWpiptHwNh491gY38->_0xb34519[whiQ5] = fJgdXHalMfeAVFwePOoI uVO1Qalnv5iBgS7nlmK9  HDKSP
+//1 ligne
+JHrXMJSgdCMA4OjS6sT bookaGet kIuIgv2Ec0s8UnWCyauq DyCzvGuO1CTg1CkpWRX0 fJgdXHalMfeAVFwePOoI, pFaZ1NS2 smIdh8p47l23w2dzqR2m GAeiCZduVBOeLgAvhRvg  oUVhQ
+    hEiM8zGRYe fJgdXHalMfeAVFwePOoI->_0xb34519[smIdh8p47l23w2dzqR2m] SwtUV  HDKSP
+//7 lignes
+pFaZ1NS2 RrQlSvRxJluemLp kIuIgv2Ec0s8UnWCyauq qAYX4nd8Rb qGnvGE3P7vC2hbPRDU9f, JHrXMJSgdCMA4OjS6sT vFrHnDaB8j9riC3ZrGDA GAeiCZduVBOeLgAvhRvg  oUVhQ
+    sWHYfMcmyBNcWNK rA07ovND = 0x64 - 0b01100101 gY8GGIzIQZF45mZ61QfJ
+    GXOERJfGJkjrQih UuMr6w23 = 0xDDA - 0x9C4 - 0b0000010000010110 gY8GGIzIQZF45mZ61QfJ
+    wFwBvQhzCHMkirj7 kIuIgv2Ec0s8UnWCyauq qGnvGE3P7vC2hbPRDU9f->_0xb34519[UuMr6w23] != NULL GAeiCZduVBOeLgAvhRvg  oUVhQ UuMr6w23++ SwtUV  rA07ovND++ uVO1Qalnv5iBgS7nlmK9  if kIuIgv2Ec0s8UnWCyauq qGnvGE3P7vC2hbPRDU9f->_0xb34519[rA07ovND] == vFrHnDaB8j9riC3ZrGDA GAeiCZduVBOeLgAvhRvg  break SwtUV HDKSP
+    Klm9i2uEL7 rA07ovND pxexKwpiTjCuwzQM
+//8 lignes
+pyWRUyKz bUn7lgtWyIXF9K6JK09O kIuIgv2Ec0s8UnWCyauq AILyGSaIaFLWs4kSC1p1 IRIVKuTocuyRmslTFppt, GXOERJfGJkjrQih m1MxkHkriTc9d1xwVcV5, JHrXMJSgdCMA4OjS6sT ZFJA89yD3jf8Dd8LoTBQ GAeiCZduVBOeLgAvhRvg  oUVhQ
+    W54itc2aQA  kIuIgv2Ec0s8UnWCyauq kIuIgv2Ec0s8UnWCyauq PWL IRIVKuTocuyRmslTFppt GAeiCZduVBOeLgAvhRvg._0xb34519[m1MxkHkriTc9d1xwVcV5] != NULL GAeiCZduVBOeLgAvhRvg  CBoAUqi0k7 kIuIgv2Ec0s8UnWCyauq IRIVKuTocuyRmslTFppt, m1MxkHkriTc9d1xwVcV5+1, IRIVKuTocuyRmslTFppt->_0xb34519[m1MxkHkriTc9d1xwVcV5] GAeiCZduVBOeLgAvhRvg  SwtUV
+    IRIVKuTocuyRmslTFppt->_0xb34519[m1MxkHkriTc9d1xwVcV5] MLldsJqmq62yDYpKuP9X ZFJA89yD3jf8Dd8LoTBQ pxexKwpiTjCuwzQM
+//2 lignes
+pyWRUyKz WQqLD3YU9E kIuIgv2Ec0s8UnWCyauq AILyGSaIaFLWs4kSC1p1 Qszlq20FJ8lioMWKmSmN, sWHYfMcmyBNcWNK XW72Jqx74hZ0CDSHPlaD GAeiCZduVBOeLgAvhRvg  oUVhQ
+    meWLrjd0nr  kIuIgv2Ec0s8UnWCyauq Qszlq20FJ8lioMWKmSmN->_0xb34519[XW72Jqx74hZ0CDSHPlaD+1] != KSr4FIYcOI3YF4URXnzy GAeiCZduVBOeLgAvhRvg  oUVhQ
+        Qszlq20FJ8lioMWKmSmN->_0xb34519[XW72Jqx74hZ0CDSHPlaD] = Qszlq20FJ8lioMWKmSmN->_0xb34519[XW72Jqx74hZ0CDSHPlaD+1] gY8GGIzIQZF45mZ61QfJ
+        SDBsAVbrbdlOACs8FB9 kIuIgv2Ec0s8UnWCyauq Qszlq20FJ8lioMWKmSmN, XW72Jqx74hZ0CDSHPlaD+1 GAeiCZduVBOeLgAvhRvg  uVO1Qalnv5iBgS7nlmK9  HDKSP
+    VSjteJVLmYrsEbdnwoTV Qszlq20FJ8lioMWKmSmN->_0xb34519[XW72Jqx74hZ0CDSHPlaD] = KSr4FIYcOI3YF4URXnzy gY8GGIzIQZF45mZ61QfJ  HDKSP
+//1 lignes
+pyWRUyKz bookaRemoveLast kIuIgv2Ec0s8UnWCyauq DyCzvGuO1CTg1CkpWRX0 this GAeiCZduVBOeLgAvhRvg  oUVhQ
+    SDBsAVbrbdlOACs8FB9 kIuIgv2Ec0s8UnWCyauq this, D8zMP kIuIgv2Ec0s8UnWCyauq this GAeiCZduVBOeLgAvhRvg + (0x64 - 0b01100101 IOiDvOT2CQn GAeiCZduVBOeLgAvhRvg  gY8GGIzIQZF45mZ61QfJ  HDKSP
+//1 ligne
+pyWRUyKz bookaSet kIuIgv2Ec0s8UnWCyauq AILyGSaIaFLWs4kSC1p1 holifk42OfsXgildz0fH, GXOERJfGJkjrQih Kamn0mBEjMZOejL8ZJKX, JHrXMJSgdCMA4OjS6sT IpI9AbgNGY1DNWO5UB8M GAeiCZduVBOeLgAvhRvg  oUVhQ
+    kIuIgv2Ec0s8UnWCyauq PWL holifk42OfsXgildz0fH GAeiCZduVBOeLgAvhRvg._0xb34519[Kamn0mBEjMZOejL8ZJKX] = IpI9AbgNGY1DNWO5UB8M SwtUV  HDKSP
+//1 ligne
+sWHYfMcmyBNcWNK D8zMP kIuIgv2Ec0s8UnWCyauq qAYX4nd8Rb bANSX GAeiCZduVBOeLgAvhRvg  oUVhQ
+    GXOERJfGJkjrQih etJ6GW n3tI5vcrIPUuCKlUvu7i zz0 SwtUV
+    r5UiRD67pBW2eNKY kIuIgv2Ec0s8UnWCyauq kIuIgv2Ec0s8UnWCyauq PWL bANSX GAeiCZduVBOeLgAvhRvg._0xb34519[etJ6GW] != KSr4FIYcOI3YF4URXnzy GAeiCZduVBOeLgAvhRvg  etJ6GW++ uVO1Qalnv5iBgS7nlmK9
+    Klm96i2EL7 etJ6GW pxexKwpiTjCuwzQM
